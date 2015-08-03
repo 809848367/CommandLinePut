8e0e3423d8483427a1eeacdd4e2709f0d2256ba5
Hello <mac@YZP.local>
2015-08-03 08:59:27 +0800

    Initial Commit

---
+++ CommandLinePut/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CommandLinePut
+//
+//  Created by Hello on 15/8/3.
+//  Copyright (c) 2015年 智游集团. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
